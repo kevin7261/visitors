4b2510d7e8440db32ea328be0cce0a23a1468b52
smilekevin <kevin7261@gmail.com>
2019-03-25 01:40:43 +0800

    Add files via upload

--- visitors/about_me.js
+++ visitors/about_me.js
@@ -17,14 +17,4 @@ function funcDraw_AboutMe_Main() {
 			.attr("id", "div_id_about_me_content")	
 			.attr("class", "div_class_about_me font_size_12 color_main")	
 				.text("smilekevin");
-
-	d3.select("#div_id_main")
-		.append("div")	
-			.attr("id", "div_id_about_me_content")	
-			.attr("class", "div_class_about_me font_size_12 color_main")	
-				.text("Data Source : https://www.kaggle.com/ceshine/visitors-to-taiwan-by-purpose/");
-
-
-
-
 }
